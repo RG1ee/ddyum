a481c87cead0c39938735a1325e505ef78b2f82a
RG1ee <niwows@yandex.ru>
2023-09-11 15:01:28 +0300

    create docker compose for database, create settings for database

---
+++ src/config/db.py
@@ -0,0 +1,13 @@
+from sqlalchemy.ext.asyncio.session import async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.ext.asyncio import create_async_engine
+
+from src.config.settings import settings
+
+
+engine = create_async_engine(f"{settings.POSTGRES_DSN}")
+async_session = async_sessionmaker(engine, autoflush=False)
+
+
+class Base(DeclarativeBase):
+    ...
