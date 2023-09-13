bc4f6064e02396ac01ee410d2db93f022baf5fc7
RG1ee <niwows@yandex.ru>
2023-09-14 00:03:06 +0300

    create user migrations

--- src/users/models.py
+++ src/users/models.py
@@ -7,8 +7,14 @@ class User(Base):
     __tablename__ = "user"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str]
+    email: Mapped[str] = mapped_column(unique=True)
     hashed_password: Mapped[str]
-    telegram: Mapped[str] = mapped_column(nullable=True)
-    phone: Mapped[str] = mapped_column(nullable=True)
+    telegram: Mapped[str | None]
+    phone: Mapped[str | None]
     is_active: Mapped[bool] = mapped_column(default=False)
+
+    def __str__(self) -> str:
+        return f"{__class__.__name__} {self.email!r}"
+
+    def __repr__(self) -> str:
+        return str(self)
