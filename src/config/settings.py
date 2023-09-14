956891d2f7bbd0c8151c9470e052eef6fae30feb
RG1ee <niwows@yandex.ru>
2023-09-14 15:07:31 +0300

    install python-jose, add to setting secret key, algorithms

--- src/config/settings.py
+++ src/config/settings.py
@@ -9,6 +9,11 @@ class Settings(BaseSettings):
         default="postgresql+asyncpg://postgres:postgres@127.0.0.1:5432/postgres",
     )
     BROKER_URL: str = Field(default="redis://localhost:6379/0")
+    ACCESS_EXPIRE: int = Field(default=10)
+    REFRESH_EXPIRE: int = Field(default=10080)
+    SECRET_KEY: str = Field(default="secret")
+    ALGORITHM: str = Field(default="HS256")
+
     model_config = SettingsConfigDict(
         env_file=".env",
         env_prefix="APP_",
