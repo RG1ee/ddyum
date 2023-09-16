4e6b7c9578f0e499aac4e780667ee0a7019c5f30
RG1ee <niwows@yandex.ru>
2023-09-16 17:36:54 +0300

    add corsheaders

--- src/main.py
+++ src/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.auth.routers import router as auth_router
 
 
 app = FastAPI(title="API FOR BOOKING PHOTO SHOOTS")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router)
