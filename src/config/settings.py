3f489208936967a719804e56782742fd269acc68
RG1ee <niwows@yandex.ru>
2023-09-17 15:03:39 +0300

    add consts for smtp server, consts for send email

--- src/config/settings.py
+++ src/config/settings.py
@@ -15,13 +15,24 @@ class Settings(BaseSettings):
         default="postgresql+asyncpg://postgres:postgres@127.0.0.1:5432/postgres",
     )
     TEST_DATABASE: str = Field(
-        default="sqlite+aiosqlite:///db.sqlite3",
+        default=f"sqlite+aiosqlite:///{BASE_DIR}/db.sqlite3",
     )
     BROKER_URL: str = Field(default="redis://localhost:6379/0")
     ACCESS_EXPIRE: int = Field(default=10)
     REFRESH_EXPIRE: int = Field(default=10080)
+    EMAIL_TOKEN_EXPIRE: int = Field(default=120)
     SECRET_KEY: str = Field(default="secret")
     ALGORITHM: str = Field(default="HS256")
+    BASE_URL: str = Field(default="http://127.0.0.1:8000/")
+
+    # Smtp server
+    SMTP_SERVER: str = Field(default="smtp.example.com")
+    SMTP_PORT: int = Field(default=465)
+    SMTP_LOGIN: str = Field(default="example@example.com")
+    SMTP_PASSWORD: str = Field(default="example")
+
+    # Email
+    SUBJECT: str = Field(default="Your Subject")
 
     model_config = SettingsConfigDict(
         env_file=".env",
