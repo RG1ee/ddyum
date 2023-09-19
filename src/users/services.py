4f7155c611ff79b55f124e4a81508729504f4c65
RG1ee <niwows@yandex.ru>
2023-09-19 20:31:33 +0300

    create method for update user

--- src/users/services.py
+++ src/users/services.py
@@ -1,7 +1,16 @@
-from src.users.models import User
+from sqlalchemy import update
 
+from src.users.models import User
 from src.services.base import BaseService
+from src.database.db import async_session
 
 
 class UserService(BaseService):
     model = User
+
+    @classmethod
+    async def update_user(cls, user_id, **data):
+        async with async_session() as session:
+            stmt = update(cls.model).filter_by(id=user_id).values(**data)
+            await session.execute(stmt)
+            await session.commit()
