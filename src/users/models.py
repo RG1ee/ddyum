7f6bed024f1c0463b7cabbfd51024583d3a6a882
Roman Lee (kittzy) <105285331+RG1ee@users.noreply.github.com>
2023-09-25 18:15:59 +0300

    create model for user profile and refactoring mock data for tests (#23)

--- src/users/models.py
+++ src/users/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.database import Base
 
@@ -9,12 +10,33 @@ class User(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(unique=True)
     hashed_password: Mapped[str]
-    telegram: Mapped[str | None]
-    phone: Mapped[str | None]
     is_active: Mapped[bool] = mapped_column(default=False)
+    is_admin: Mapped[bool] = mapped_column(default=False)
+
+    profile: Mapped["Profile"] = relationship(
+        uselist=False,
+        backref="profile",
+    )
 
     def __str__(self) -> str:
         return f"{__class__.__name__} {self.email!r}"
+
+    def __repr__(self) -> str:
+        return str(self)
+
+
+class Profile(Base):
+    __tablename__ = "profile"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    telegram: Mapped[str | None]
+    phone: Mapped[str | None]
+
+    user_profile: Mapped["User"] = relationship(backref="user")
+
+    def __str__(self) -> str:
+        return f"{__class__.__name__} {self.telegram!r}"
 
     def __repr__(self) -> str:
         return str(self)
