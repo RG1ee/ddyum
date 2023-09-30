ff95cddeb9b4672e5e58fabbbff16f0dfeb22660
Roman Lee (kittzy) <105285331+RG1ee@users.noreply.github.com>
2023-09-30 13:23:32 +0300

    add fields - first name, telegram to registration endpoint (#28)

--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -3,13 +3,14 @@ from pydantic import Field, EmailStr
 from src.base.schemas import BaseConfigSchema
 
 
-class AuthUserRegistrationSchema(BaseConfigSchema):
+class AuthUserLoginSchema(BaseConfigSchema):
     email: EmailStr = Field(...)
     password: str = Field(..., min_length=8)
 
 
-class AuthUserSchema(AuthUserRegistrationSchema):
-    pass
+class AuthUserRegistrationSchema(AuthUserLoginSchema):
+    first_name: str = Field(..., max_length=100)
+    telegram: str = Field(..., max_length=50)
 
 
 class AuthUserRefreshSchema(BaseConfigSchema):
--- src/tests/test_auth/test_api.py
+++ src/tests/test_auth/test_api.py
@@ -6,21 +6,29 @@ from src.config.settings import settings
 
 
 @pytest.mark.parametrize(
-    "email, password, status_code",
+    "email, password, status_code, first_name, telegram",
     [
-        ("test@email.com", "testpassword", 201),
-        ("test@email.com", "testpassword1", 400),
+        ("test@email.com", "testpassword", 201, "Kolya", "kolya"),
+        ("test@email.com", "testpassword1", 400, "Petya", "petya"),
     ],
 )
 async def test_register_user(
     email: str,
     password: str,
+    first_name: str,
+    telegram: str,
     status_code: int,
     client: AsyncClient,
 ):
+    data = {
+        "email": email,
+        "password": password,
+        "firstName": first_name,
+        "telegram": telegram,
+    }
     response = await client.post(
         f"{settings.API_PREFIX}/auth/register",
-        json={"email": email, "password": password},
+        json=data,
     )
 
     assert response.status_code == status_code
